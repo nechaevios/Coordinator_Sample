239b11b264e9e602b791ee1febae0627a5be6714
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-02-22 22:13:29 +0300

    fix linter rules, add appCoordinator

--- Coordinator_Sample/App/SceneDelegate.swift
+++ Coordinator_Sample/App/SceneDelegate.swift
@@ -10,12 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MainViewController()
+        let navigationController: UINavigationController = .init()
+        window.rootViewController = navigationController
+
         window.makeKeyAndVisible()
+
+        appCoordinator = AppCoordinator(navigationController)
+        appCoordinator?.start()
         self.window = window
     }
 
