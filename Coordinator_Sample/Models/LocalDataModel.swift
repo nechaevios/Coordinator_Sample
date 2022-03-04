1b8f29e99e553631fcc86f11f16deb22b1dcb979
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-03-04 21:47:16 +0300

    LocalUserSettings data model added

---
+++ Coordinator_Sample/Models/LocalDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocalDataModel.swift
+//  Coordinator_Sample
+//
+//  Created by Nechaev Sergey  on 23.02.2022.
+//
+
+import Foundation
+
+struct LocalUserSettings {
+    let age: Int
+    let isActive: Bool
+}
