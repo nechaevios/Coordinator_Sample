bb5da4264dc954400e17ffbf263351b66748c232
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-03-06 15:44:52 +0300

    ReadyViewModel added

---
+++ Coordinator_Sample/Content Views/ReadyView/Main/ReadyViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ReadyViewModel.swift
+//  Coordinator_Sample
+//
+//  Created by Nechaev Sergey  on 23.02.2022.
+//
+
+import Foundation
+
+protocol ReadyViewModelProtocol {
+
+}
+
+class ReadyViewModel: ReadyViewModelProtocol {
+
+}
